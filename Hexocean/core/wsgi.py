6546cea12a378fcb3587084cf8270ddd1ac08377
Alaskowy <alaskmp@gmail.com>
2023-02-20 10:37:35 +0100

    readme.md fix

--- Hexocean/core/wsgi.py
+++ Hexocean/core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Hexocean project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hexocean.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
